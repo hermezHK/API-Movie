6f473e3b44804f9c415529ba00e42de82c6968c7
Hermez <angelo_stk06@hotmail.com>
2023-03-02 14:05:16 -0500

    add model and create api

---
+++ movie/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Movie(models.Model):
+    #id es autogenerado por django
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    year = models.IntegerField()
+    rating = models.FloatField()
+    genre = models.CharField(max_length=50)
+    image = models.TextField()
+    country = models.CharField(max_length=50)
+    duration = models.IntegerField()
+    price = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        db_table = "movies"
